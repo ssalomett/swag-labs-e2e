dc1b1bf26d65c05206d733b7e62a18a5e4eef2c9
ssalomett <tusniashvilis@gmail.com>
2026-07-26 23:33:42 +0400

    Update tests and screenshots

--- cypress/e2e/01-login.cy.js
+++ cypress/e2e/01-login.cy.js
@@ -11,7 +11,19 @@ describe('login tests', () => {
       if (user.username === 'standard_user' && user.password === 'secret_sauce') {
         // ვალიდური მონაცემები
         cy.url().should('include', user.expectedMessage)
-      } else {
+      } 
+      else if (user.username === 'problem_user') {
+        // problem_user-ით შესვლა და ბაგების ტესტირება
+        cy.url().should('include', user.expectedMessage)
+
+        // ბაგი 1: სურათების შემოწმება
+        cy.get('.inventory_item_img img')
+          .first()
+          .should('have.attr', 'src')
+          .and('not.include', 'sl-404')
+
+      }
+      else {
         // არავალიდური მონაცემები
         cy.get('[data-test="error"]').should('contain', user.expectedMessage)
       }
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,1 +1,9 @@
 import './commands'
+
+afterEach(function () {
+  // თუ მიმდინარე ტესტი ჩავარდა (failed)
+  if (this.currentTest && this.currentTest.state === 'failed') {
+    // გადაიღე სქრინშოტი და დაარქვი ტესტის სახელი
+    cy.screenshot(`failed-${this.currentTest.title}`)
+  }
+})
